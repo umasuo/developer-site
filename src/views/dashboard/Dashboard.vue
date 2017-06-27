31cf34454550dc20fcfddd160bb425e5c1e2fdfb
Alan Zhang <gagazcfan@icloud.com>
2017-06-27 10:44:55 +0800

    no message

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -2,52 +2,53 @@
   <div>
     <div class="row tile_count">
       <div class="col-md-3 col-sm-6 tile_stats_count">
-        <span class="count_top"><i class="fa fa-user"></i> 新用户反馈</span>
-        <div class="count">2344238</div>
+        <span class="count_top"><i class="fa fa-user"></i> 新激活设备（今日）</span>
+        <div class="count">290</div>
       </div>
       <div class="col-md-3 col-sm-6 tile_stats_count">
-        <span class="count_top"><i class="fa fa-clock-o"></i> 新激活设备（今日）</span>
-        <div class="count">4340</div>
+        <span class="count_top"><i class="fa fa-clock-o"></i> 在线设备</span>
+        <div class="count green">43409</div>
       </div>
       <div class="col-md-3 col-sm-6 tile_stats_count">
-        <span class="count_top"><i class="fa fa-user"></i> 在线设备数（今日）</span>
-        <div class="count green">290</div>
+        <span class="count_top"><i class="fa fa-user"></i> 新增用户（今日）</span>
+        <div class="count">230</div>
       </div>
       <div class="col-md-3 col-sm-6 tile_stats_count">
-        <span class="count_top"><i class="fa fa-user"></i> 新增激活用户（今日）</span>
+        <span class="count_top"><i class="fa fa-user"></i> 新用户反馈（今日）</span>
         <div class="count">4,567</div>
       </div>
     </div>
 
     <div class="row">
-      <div class="col-sm-12">
-        <form class="form-inline">
-          <div class="form-group">
-            <label>选择设备类型：
+      <div class="col-xs-12">
+        <div class="x_panel">
+          <div class="x_content">
+
+            <form class="form-inline">
+              <div class="form-group">
+                <label>选择设备类型：
+                  <select class="form-control">
+                    <option>设备1</option>
+                    <option>设备1</option>
+                    <option>设备1</option>
+                  </select>
+                </label>
+              </div>
+
+              <div class="form-group">
+                <label>选择时间周期：
               <select class="form-control">
-                <option>设备1</option>
-                <option>设备1</option>
-                <option>设备1</option>
-              </select>
-            </label>
+                  <option>过去7天</option>
+                  <option>过去一个月</option>
+                  <option>过去三个月</option>
+                </select>
+                </label>
+              </div>
+
+              <button class="form-control btn-default">查询</button>
+            </form>
           </div>
-
-          &nbsp;
-
-          <div class="form-group">
-            <label>选择时间周期：
-          <select class="form-control">
-              <option>过去7天</option>
-              <option>过去一个月</option>
-              <option>过去三个月</option>
-            </select>
-            </label>
-          </div>
-
-          &nbsp;
-
-          <button class="form-control btn-default">查询</button>
-        </form>
+        </div>
       </div>
     </div><!-- end of search condition -->
 
