b3f2111206a53d25c0217dc1300b7ed23229cc3f
Alan Zhang <gagazcfan@icloud.com>
2017-06-27 16:26:32 +0800

    remove useless header

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -22,11 +22,6 @@
     <div class="row">
       <div class="col-xs-12">
         <div class="x_panel">
-          <div class="x_title">
-            <h2>过滤条件</h2>
-            <div class="clearfix"></div>
-          </div>
-
           <div class="x_content">
 
             <form class="form-inline">
