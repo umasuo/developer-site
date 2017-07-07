3b4bcd747936dae2b0ba6369370beb64fe7b741a
Alan Zhang <gagazcfan@icloud.com>
2017-07-07 09:58:02 +0800

    email verify is done

--- src/views/EmailVerifyResult.vue
+++ src/views/EmailVerifyResult.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="eva-single-page-wrapper text-center">
-    <h1 class="eva-line-through-title">验证成功</h1>
+    <template v-if="result === 'success'">
+      <h1 class="eva-line-through-title">验证成功</h1>
 
-    <p>您的邮箱已经验证成功，请 <router-link :to="{name:'Login'}">前往登陆</router-link></p>
+      <p>您的邮箱已经验证成功，请 <router-link :to="{name:'Login'}">前往登陆</router-link></p>
+    </template>
+
+    <template v-if="result === 'failed'">
+      <h1 class="eva-line-through-title">验证错误</h1>
+
+      <p>您的邮箱验证失败，可能是由验证链接过期或复制不完整导致，请检查链接是否复制正确，或于登录后点击右上角头像重发。 <router-link :to="{name:'Login'}">前往登陆</router-link></p>
+    </template>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'EmailVerifyResult'
+    name: 'EmailVerifyResult',
+
+    computed: {
+      result () {
+        // result query param should be one of: success, failed
+        const verifyResult = this.$route.query.result
+
+        if (verifyResult) {
+          return verifyResult
+        } else {
+          return 'failed'
+        }
+      }
+    }
   }
 </script>
 
