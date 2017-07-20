37f301dec17032ab6def638ad724f0ee60ad3ac0
Alan Zhang <gagazcfan@icloud.com>
2017-07-20 18:10:02 +0800

    enable compression middleware

--- build/docker/server.js
+++ build/docker/server.js
@@ -1,7 +1,9 @@
 var path = require('path')
 var express = require('express')
+var compression = require('compression');
 var app = express()
 
+app.use(compression())
 app.use(express.static('./'))
 
 app.use('/', function ( req, res, next ) {
